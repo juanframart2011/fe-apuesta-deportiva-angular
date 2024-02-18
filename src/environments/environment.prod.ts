e3b1fc9b70dde817e55f50442ad7f501a021d0d0
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-02-17 18:50:22 -0600

    proyecto modular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://186.4.251.123:8083/deportivo/api/deportivo/', // Ejemplo de URL de la API para desarrollo
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://186.4.251.123:8083/deportivo/api/deportivo/', // Ejemplo de URL de la API para desarrollo
+  };
